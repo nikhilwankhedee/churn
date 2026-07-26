2cc42f8380b81fdea6f05bf86faffa548b97af6a
Nikhil Wankhedee <tonywankhedee@gmail.com>
2026-07-26 16:00:00 +0530

    experiments: experiment lifecycle manager

---
+++ src/experiments/__init__.py
@@ -0,0 +1,4 @@
+"""Experiment Management: track, list, and reproduce runs."""
+from src.experiments.manager import ExperimentManager, ExperimentRecord
+
+__all__ = ["ExperimentManager", "ExperimentRecord"]
