b5ff8fc813794b51300f8bbe41ae4843a6190e09
Nikhil Wankhedee <tonywankhedee@gmail.com>
2026-08-05 11:00:00 +0530

    downloads: dataset download manager

---
+++ src/downloads/__init__.py
@@ -0,0 +1,4 @@
+"""Download Manager: obtain benchmark datasets."""
+from src.downloads.manager import list_datasets as list_benchmark_datasets, get_dataset_info
+
+__all__ = ["list_benchmark_datasets", "get_dataset_info"]
